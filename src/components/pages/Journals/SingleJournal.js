4ddd7deb65605c8cbd936e716b5d8e58320c606b
unknown <sikhaadhi2311@gmail.com>
2022-06-26 02:49:40 +0530

    added popup

--- src/components/pages/Journals/SingleJournal.js
+++ src/components/pages/Journals/SingleJournal.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import JournalsContext from "../../../context/journals/JournalsContext";
@@ -5,14 +6,42 @@ import {FiEdit3} from 'react-icons/fi'
 import {MdDeleteOutline} from 'react-icons/md'
 import {RiShareForwardLine} from 'react-icons/ri'
 import {HiOutlineStar,HiStar} from 'react-icons/hi'
+import {GrFormView} from 'react-icons/gr'
 import Wrapper from "../../../assets/wrappers/SingleJournal";
 import AlertContext from "../../../context/alert/AlertContext";
-
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 const SingleJournal = ({journal,isShared}) => {
   const {setAlert}=useContext(AlertContext)
-  const {_id,title,content,dateCreated,category,lastModified,isFavorite}=journal
+  const {_id,title,content,dateCreated,category,lastModified}=journal
   const { setEditJournal,deleteJournal,toggleFavorite,toggleButton} = useContext(JournalsContext);
- 
+  
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
   const handleDelete=()=>{
     try{
     deleteJournal(_id);
@@ -25,54 +54,83 @@ const SingleJournal = ({journal,isShared}) => {
   const handleFavorite=()=>{
     toggleFavorite(_id)
   }
-  console.log(isShared)
   return (
     <Wrapper>
+  <header>
+    <div className="info">
+      <h5>{title}</h5>
+      <p>category:{category}</p>
+      <p>last modified:{lastModified}</p>
+    </div>
+  </header>
+  <div className="content">
+    <p>{content.substring(0, 100)}....
+    <Button
+      onClick={handleClickOpen("paper")}
+    >READ MORE</Button>
+    </p>
+  </div>
+  <Dialog
+    open={open}
+    onClose={handleClose}
+    scroll={scroll}
+    aria-labelledby="scroll-dialog-title"
+    aria-describedby="scroll-dialog-description"
+  >
+    <DialogTitle id="scroll-dialog-title">
       <header>
         <div className="info">
           <h5>{title}</h5>
-          <p>category:{category}</p>
-          <p>last modified:{lastModified}</p>
         </div>
       </header>
-      <div className="content">
-        <p>{content}</p>
-        {isShared&&
-          <footer>
-          <div className="actions">
-            <Link
-              to="/edit-journal"
-              onClick={() => setEditJournal(_id)}
-              className="btn edit-btn"
-            >
-              <FiEdit3/>
-            </Link>
-            <button
-              type="button"
-              className="btn delete-btn"
-              onClick={handleDelete}
-            >
-             <MdDeleteOutline/>
-            </button>
+    </DialogTitle>
+    <DialogContent dividers={scroll === "paper"}>
+      <DialogContentText
+        id="scroll-dialog-description"
+        ref={descriptionElementRef}
+        tabIndex={-1}
+      >
+        <div className="content">
+          <p>{content}</p>
+        </div>
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} className="btn close-btn">Close</Button>
+    </DialogActions>
+  </Dialog>
+  <div className="content">
+  {isShared && (
+    <footer>
+      <div className="actions">
+        <Link
+          to="/edit-journal"
+          onClick={() => setEditJournal(_id)}
+          className="btn edit-btn"
+        >
+          <FiEdit3 />
+        </Link>
+        <button type="button" className="btn delete-btn" onClick={handleDelete}>
+          <MdDeleteOutline />
+        </button>
 
-            {/* set sharing here */}
-            <Link 
-              to="/share-journal"
-              onClick={() => setEditJournal(_id)}
-              className="btn share-btn">
-            <RiShareForwardLine/>
-            </Link>
+        {/* set sharing here */}
+        <Link
+          to="/share-journal"
+          onClick={() => setEditJournal(_id)}
+          className="btn share-btn"
+        >
+          <RiShareForwardLine />
+        </Link>
 
-           <button className="btn starred-btn" onClick={handleFavorite}>
-            {!toggleButton?<HiOutlineStar/>:<HiStar/>}
-           </button>
-
-          </div>
-        </footer>
-      }
-        
+        <button className="btn starred-btn" onClick={handleFavorite}>
+          {!toggleButton ? <HiOutlineStar /> : <HiStar />}
+        </button>
       </div>
-    </Wrapper>
+    </footer>
+  )}
+  </div>
+</Wrapper>
   );
 }
 export default SingleJournal;
